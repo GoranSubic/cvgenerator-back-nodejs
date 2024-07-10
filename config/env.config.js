ff26e8d52ccbf1f7ab7a48d95e30b193bb1e880a
Goran Subic <goran.subic@actdigital.com>
2024-07-10 15:29:36 +0200

    Env, db init.

--- config/env.config.js
+++ config/env.config.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const env = process.env;
 
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USERNAME,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_DATABASE,
-    port: process.env.FORWARD_DB_PORT,
+    HOST: env.DB_HOST,
+    USER: env.DB_USERNAME,
+    PASSWORD: env.DB_PASSWORD,
+    DB: env.DB_DATABASE,
+    port: env.FORWARD_DB_PORT,
     dialect: "postgres",
     pool: {
       max: 5,
@@ -14,6 +14,6 @@ module.exports = {
       acquire: 30000,
       idle: 10000
     },
-    local_node_port: process.env.LOCAL_NODE_PORT,
-    docker_node_port: process.env.DOCKER_NODE_PORT
+    local_node_port: env.LOCAL_NODE_PORT,
+    docker_node_port: env.DOCKER_NODE_PORT
   };
